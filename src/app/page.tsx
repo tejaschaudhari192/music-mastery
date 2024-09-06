ae94e96fbe19885217a912a544c1baa660021376
jaybalaji <jaybalaji192@gmail.com>
2024-09-06 23:58:45 +0530

    tooltip added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import FeaturedCourses from "@/components/FeaturedCourses";
 import WhyChooseUs from "@/components/WhyChooseUs";
 import SchoolTestimonials from "@/components/TestimonialCards";
 import UpcomingWebinars from "@/components/UpcomingWebinars";
+import Instructors from "@/components/Instructors";
 
 export default function Home() {
     return (
@@ -13,6 +14,7 @@ export default function Home() {
             <WhyChooseUs/>
             <SchoolTestimonials/>
             <UpcomingWebinars/>
+            <Instructors/>
         </main>
     );
 }
