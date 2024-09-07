88434a23924ce1f31d44320a269f0a56219d4d2e
jaybalaji <jaybalaji192@gmail.com>
2024-09-07 07:55:34 +0530

    footer added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import WhyChooseUs from "@/components/WhyChooseUs";
 import SchoolTestimonials from "@/components/TestimonialCards";
 import UpcomingWebinars from "@/components/UpcomingWebinars";
 import Instructors from "@/components/Instructors";
+import Footer from "@/components/Footer";
 
 export default function Home() {
     return (
@@ -15,6 +16,7 @@ export default function Home() {
             <SchoolTestimonials/>
             <UpcomingWebinars/>
             <Instructors/>
+            <Footer/>
         </main>
     );
 }
